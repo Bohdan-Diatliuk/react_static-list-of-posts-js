2e82706cc11132ded4e01694dcec7ce3e86790a2
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-09-30 23:21:45 +0300

    Solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,11 @@
-export const UserInfo = () => <>Put the user here</>;
+export const UserInfo = ({ user }) => {
+  if (!user) {
+    return <span className="UserInfo">Uknown</span>;
+  }
+
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
